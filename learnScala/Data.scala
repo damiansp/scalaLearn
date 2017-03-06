22a4f8b1b54c02d4051be6445144d58a1217acfd
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2017-03-05 22:03:42 -0500

    Initialize Data.scala with basic examples

---
+++ learnScala/Data.scala
@@ -0,0 +1,11 @@
+val x: Int = 5;
+x * 2;
+x / 5;
+res0 + res1; // in REPL will add res(ults) of previous commands
+
+var a: Double = 7.27;
+a = 355.0 / 113.0;
+a = 5;
+
+val greeting: String = "Oh so stringy";
+val at: Char = '@';
