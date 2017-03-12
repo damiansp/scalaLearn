11d956777504b3a593ae7ddef07a96e3db15f8a8
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2017-03-11 23:00:10 -0500

    more match examples

--- learnScala/Match.scala
+++ learnScala/Match.scala
@@ -27,3 +27,31 @@ val daytype = day match {
   case "SAT" | "SUN" => "weekend"
 };
 
+// Default/wildcard matching
+val msg = "Unauthorized";
+val status = msg match {
+  case "Ok" => 200
+  case _ => {
+    println(s"Couldn't parse $message")
+    -1
+  }
+};
+
+// Matching with pattern guards
+val response: String = null;
+response match {
+  case s if s != null => println(s"Received '$s'")
+  case s              => println("Error! Received a null response")
+};
+
+// Matching types with pattern variables
+val x: Int = 12180;
+val y: Any = x;
+
+y match {
+  case x: String => s"'$x'"
+  case x: Double => f"$x%.2f"
+  case x: Float  => f"$x%.2f"
+  case x: Long   => s"${x}l"
+  case x: Int    => s"${x}i"
+};
