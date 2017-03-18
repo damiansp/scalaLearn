3616138d28d523d8046a1048b7aa0938062f2c3a
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2017-03-18 00:23:18 -0400

    added recursive and nested examples

--- learnScala/Functions.scala
+++ learnScala/Functions.scala
@@ -7,7 +7,7 @@ def hi2() = "hi";
  */
 
 def multiplier(x: Int, y: Int): Int = { x * y; };
-println(multipler(6, 7));
+println(multiplier(6, 7));
 
 def safeTrim(s: String): String = {
   if (s == null) return null;
@@ -15,4 +15,43 @@ def safeTrim(s: String): String = {
 };
 
 def log(d: Double): Unit = println(f"Got value $d%.2f");
+def formatEuro(amt: Double) = f"€$amt%.2f";
 
+println(formatEuro(3.14159));
+println(formatEuro {
+    val rate = 1.32;
+    0.235 + 0.7123 + rate * 5.32 });
+
+// Recursive functions
+def power(x: Int, n: Int): Long = {
+  if (n >= 1) x * power(x, n - 1) else 1;
+};
+
+println(power(2, 8));
+
+/**
+ * @annotation.tailrec
+ * def pow(x: Int, n: Int): Long = {
+ *   if (n >= 1) x * pow(x, n - 1) else 1; // if return is function invocation, can be optimized
+ * };                                      // This one fails
+ *
+ * @annotation.tailrec
+ * def pow(x: Int, n: Int): Long = {
+ *   if (n < 1) 1 else x * pow(x, n - 1) // fails: call to pow() being multiplied by other value
+ * };
+ */
+
+@annotation.tailrec
+def pow(x: Int, n: Int, t: Int = 1): Int = {
+  if (n < 1) t else pow(x, n - 1, x * t)
+};
+
+println(pow(2, 8));
+
+// Nested functions
+def max(a: Int, b: Int, c: Int) = {
+  def max(x: Int, y: Int) = if (x > y) x else y;
+  max(a, max(b, c))
+};
+
+println(max(1, 2, 4));
