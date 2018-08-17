7bfeae9b5eba900de05b9c0c041834a64c39d74a
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-08-17 07:54:01 -0700

    +match SeqUnapply, VarArg

---
+++ progScala/src/main/scala/04_patternMatching/matchSeqUnapplySeq.scala
@@ -0,0 +1,22 @@
+val nonEmptyList = List(1, 2, 3, 4, 5)
+val emptyList = Nil
+val nonEmptyMap = Map("one" -> 1, "two" -> 2, "three" -> 3)
+
+def windows[T](seq: Seq[T]): String = seq match {
+	case Seq(head1, head2, _*) => s"($head1, $head2), " + windows(seq.tail)
+	case Seq(head, _*)         => s"($head, _), " + windows(seq.tail)
+	case Nil                   => "Nil"
+}
+
+for (seq <- Seq(nonEmptyList, emptyList, nonEmptyMap)) { println(windows(seq)) }
+
+
+
+def windows2[T](seq: Seq[T]): String = seq match {
+	case head1 +: head2 +: tail => s"($head1, $head2), " + windows2(seq.tail)
+	case head +: tail           => s"($head, _), " + windows2(tail)
+	case Nil                    => "Nil"
+}
+
+for (seq <- Seq(nonEmptyList, emptyList, nonEmptyMap)) { println(windows2(seq)) }
+
