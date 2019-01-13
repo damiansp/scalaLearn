a9acd8b6f95c45c88dfc047fca138fe487aa229c
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-01-12 23:17:10 -0500

    add description to Person class and filterAdult to Person Object, and test

--- projects/01_Intro/src/main/scala/Person.scala
+++ projects/01_Intro/src/main/scala/Person.scala
@@ -1,1 +1,9 @@
-case class Person(firstName: String, lastName: String, age: Int)
+case class Person(firstName: String, lastName: String, age: Int) {
+  def description = s"$firstName $lastName is $age ${if (age == 1) "year" else "years"} old"
+}
+
+object Person {
+  def filterAdult(people: List[Person]): List[Person] = {
+    people.filter(_.age >= 18)
+  }
+}
--- projects/01_Intro/src/test/scala/MainSpec.scala
+++ projects/01_Intro/src/test/scala/MainSpec.scala
@@ -8,5 +8,23 @@ class MainSpec extends WordSpec with Matchers {
       john.lastName should be("Smith")
       john.age should be(42)
     }
+    "get a human-readable representation of the person" in {
+      val paul = Person(firstName="Paul", lastName="Smith", age=24)
+      paul.description should be("Paul Smith is 24 years old")
+    }
+  }
+
+  "The Person companion object" should {
+    val (akira, peter, nick) = (Person(firstName="Akira", lastName="Sakura", age=12),
+                                Person(firstName="Peter", lastName="Müller", age=34),
+                                Person(firstName="Nick", lastName="Tagart", age=52))
+    "return a list of adults" in {
+      val ref = List(akira, peter, nick)
+      Person.filterAdult(ref) should be(List(peter, nick))
+    }
+    "return an empty list if no adult in the list" in {
+      val ref = List(akira)
+      Person.filterAdult(ref) should be(List.empty[Person])
+    }
   }
 }
