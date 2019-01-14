54dada8ac9e9f83cc4435364625109368382f79a
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-01-14 07:41:31 -0800

    add to functional

--- beginning/04_FunctionalProgramming/functional.scala
+++ beginning/04_FunctionalProgramming/functional.scala
@@ -4,5 +4,13 @@ object Functional {
 
 		val areaOfRectangle: (Int, Int) => Int = (width: Int, height: Int) => width * height
 		println(areaOfRectangle(5, 3))
+
+		val doubler = (i: Int) => { i * 2 }
+		println(doubler(2))
+
+		def operation(functionParam: (Int, Int) => Int) { println(functionParam(4, 4)) }
+		val mult = (x: Int, y: Int) => { x * y }
+		operation(add)
+		operation(mult)
 	}
 }
