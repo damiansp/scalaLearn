d7c7d90ea7ff48fbcd59a6290519d3594703f563
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-01-22 08:08:20 -0800

    add to patternMatching

--- beginning/05_PatternMatching/patternMatching.scala
+++ beginning/05_PatternMatching/patternMatching.scala
@@ -1,5 +1,23 @@
 object Functional {
   def main(args: Array[String]) {
+  	def printNum(int: Int) {
+  		int match {
+  			case 0 => println("Da big Zero")
+  			case 1 => println("The one and only")
+  			case _ => println("And anudder number")
+  		}
+  	}
 
+	 	printNum(1)
+	 	printNum(9)
+
+
+	 	def fibonacci(n: Int): Int = n match {
+	 		case 0 => 0
+	 		case 1 => 1
+	 		case n => fibonacci(n - 1) + fibonacci(n - 2)
+	 	}
+
+	 	println(fibonacci(8))
   }
 }
