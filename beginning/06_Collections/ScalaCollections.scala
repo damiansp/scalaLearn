188865923a89ad253700e60a8ea921e0956e2ca8
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-02-11 08:14:39 -0800

    add immutable

--- beginning/06_Collections/ScalaCollections.scala
+++ beginning/06_Collections/ScalaCollections.scala
@@ -6,5 +6,16 @@ object ScalaCollections {
 
 
 		// Sets
+		val s = Set(1, 2, 3)
+
+
+		// Map
+		val map = Map(1 -> "a", 2 -> "b", 3 -> "c")
+
+
+		// Immutable Seq
+		val imIndSeq = scala.collection.immutable.IndexedSeq(1, 2, 3) // = Vector
+		val imLinSeq = scala.collection.immutable.LinearSeq(1, 2, 3)
+		val imSeq = scala.collection.immutable.Seq(1, 2, 3)
 	}
 }
