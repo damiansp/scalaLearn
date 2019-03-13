32851316dc81a0d0903575fb5f68b3fa9b722a19
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-03-12 23:43:12 -0400

    init Traits

--- beginning/07_Traits/Traits.scala
+++ beginning/07_Traits/Traits.scala
@@ -1,6 +1,19 @@
-object ScalaCollections {
+object Traits {
   def main(args: Array[String]) {
+    trait Gliding {
+      def gliding() = println("gliding")
+    }
 
 
+    class Glider extends Gliding {
+      override def toString = "glider"
+      //override def gliding() = println("race for now " + toString)
+    }
+
+
+    val glider = new Glider
+    glider.gliding()
+    val g: Glider = glider
+    g.gliding()
   }
 }
