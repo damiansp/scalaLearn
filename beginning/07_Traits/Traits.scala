c0de79203e486de830d35e53e1175a8c321a7a93
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-03-17 16:59:17 -0400

    add traits and extends to Traits

--- beginning/07_Traits/Traits.scala
+++ beginning/07_Traits/Traits.scala
@@ -15,5 +15,42 @@ object Traits {
     glider.gliding()
     val g: Glider = glider
     g.gliding()
+
+
+    /**
+    trait TraitA {
+      def methodA
+      def methodAWithParam(param: String)
+      def methodWithoutReturnType: String
+    }
+
+
+    trait TraitB extends TraitA {
+      def methodB
+    }
+
+
+    class ClassA extends TraitA {
+      // ...
+    }
+
+    class ClassA extends TraitA with TraitB {
+      // ...
+    }
+
+    class ClassB extends ClassA with TraitA with TraitB {
+      // ...
+    }
+    */
+
+    trait Vehicle {
+      def drive = println("Driving")
+      def race // abstract
+    }
+
+
+    class Car extends Vehicle {
+      def race = "Racing the car"
+    }
   }
 }
