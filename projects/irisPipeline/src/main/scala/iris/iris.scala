6459c5357b8b5434be7ab64b848b1aa3d84f1d71
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-05-20 11:57:35 -0700

    init iris.scala

---
+++ projects/irisPipeline/src/main/scala/iris/iris.scala
@@ -0,0 +1,33 @@
+package iris
+
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+
+trait IrisWrapper {
+  lazy val session: SparkSession = SparkSession
+    .builder()
+    .master("local")
+    .appName("iris-pipeline")
+    .getOrCreate()
+  val dataSetPath = "/Users/dsatterthwaite/Learning/scalaLearn/projects/irisPipeline/iris.csv"
+  val irisFeaturesCategoryOrSpeciesIndexedLabel = (
+    "iris-features-column", "iris-species-column", "label")
+
+  /**
+    * Load iris data: CSV with no header
+    * @param dataSet: path to data set
+    * @return a DataFrame
+    */
+  def buildDataFrame(dataSet: String): DataFrame = {
+    def getRows(): Array[(Vector, String)] = {
+      session.sparkContext.textFile(dataSet).flatMap {
+        partitionLine => partitionLine.split("\n").toList
+      }.map(_.split(",")).collect.drop(1).map(row => (
+        Vectors.dense(row(1).toDouble, row(2).toDouble, row(3).toDouble, row(4).toDouble), row(5)))
+    }
+
+    val dataFrame = session.createDataFrame(getRows)
+      .toDF("iris-features-column", "iris-species-column")
+  }
+}
