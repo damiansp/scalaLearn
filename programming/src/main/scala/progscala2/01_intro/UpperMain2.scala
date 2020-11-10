baf070311f6733d335c0ed6b7b72c2714e0c8ac0
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-11-10 15:27:22 -0800

    revamp

--- programming/src/main/scala/progscala2/01_intro/UpperMain2.scala
+++
@@ -1,8 +0,0 @@
-package progscala3.introscala
-
-object UpperMain2 {
-  def main(params: Array[String]): Unit = {
-    val output = params.map(_.toUpperCase()).mkString(" ")
-    println(output)
-  }
-}
