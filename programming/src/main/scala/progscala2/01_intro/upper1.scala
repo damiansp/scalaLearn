847808b7e4ba4e9a273b07540627df09c07a79af
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-11-16 11:50:26 -0800

    simplify toUpper

--- programming/src/main/scala/progscala2/01_intro/upper1.scala
+++ programming/src/main/scala/progscala2/01_intro/upper1.scala
@@ -1,8 +1,10 @@
-class Upper {
+object Upper {
   def upper(strings: String*): Seq[String] = {
     strings.map((s: String) => s.toUpperCase())
   }
+
+  def upper2(strings: String*) = strings.map(_.toUpperCase())
 }
 
-val up = new Upper
-println(up.upper("hello", "world!"))
+println(Upper.upper("hello", "world!"))
+println(Upper.upper2("and", "goodbye."))
