df3a223e7342f47754a2f23a51f89d59cf15e51a
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2021-10-02 20:09:43 -0700

    methods/funcs++

--- ps/gettingStarted/7_methodsFunctions/methodsfunctions/src/main/scala/Main.scala
+++ ps/gettingStarted/7_methodsFunctions/methodsfunctions/src/main/scala/Main.scala
@@ -1,5 +1,50 @@
 @main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+  val fo = square(2)
+  println(fo)
+  val ate = mult(2, fo)
+  println(ate)
+  val sixfo = mult(2, fo, ate)
+  println(sixfo)
+  val oddsome = sumOdd(10)
+  println(oddsome)
+  greet(last="Stark", first="Tony")
+  logTransaction(100.25, true, "USD")
+  logTransaction(33.95)
+  logTransaction(88.88, false, "NZD")
 
-def msg = "I was compiled by Scala 3. :)"
+
+def square(x: Int): Int = x * x
+
+
+def mult(x: Int, y: Int): Int = x * y
+
+
+def mult(ns: Int*): Int = 
+  var prod = 1
+  for (n <- ns) prod *= n
+  prod
+
+
+def sumOdd(n: Int): Int =
+  
+  def getOdd(x: Int): Array[Int] =
+    var res = Array[Int]()
+    var cur = 1
+    while (cur <= x) {
+      if (cur % 2 == 1) res :+= cur
+      cur += 1
+    }
+    res
+
+  val odds = getOdd(n)
+  println(odds.mkString(", "))
+  odds.sum
+
+
+def greet(first: String, last: String): Unit = 
+  println("Hello! " + first + " " + last)
+
+
+def logTransaction(amt: Double, debit: Boolean = true, currency: String = "USD"): Unit = 
+  val symbol = if(debit) "-" else "+"
+  println(symbol + currency + amt)
