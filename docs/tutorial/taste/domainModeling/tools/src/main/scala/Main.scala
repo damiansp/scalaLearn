4a09e09b7fdf5a772619c69aaac05257bf670c07
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2021-11-06 10:34:02 -0700

    tools++

--- docs/tutorial/taste/domainModeling/tools/src/main/scala/Main.scala
+++ docs/tutorial/taste/domainModeling/tools/src/main/scala/Main.scala
@@ -1,3 +1,7 @@
+import java.time.*
+import scala.math.*
+
+
 @main def hello: Unit = 
   // Classes
   class Person(var name: String, var vocation: String)
@@ -20,7 +24,7 @@
       println(fullName)
 
     printFullName
-    pintln("Initialization complete")
+    println("Initialization complete")
 
   val bob = Human("Bob", "Dobolina")
   bob.printFullName
@@ -36,3 +40,53 @@
 
 
   // Aux constructors
+  class Student(var name: String, var gid: String):
+    private var _appDate: Option[LocalDate] = None
+    private var _sid: Int = 0
+
+    def this(name: String, gid: String, appDate: LocalDate) =
+      this(name, gid)
+      _appDate = Some(appDate)
+
+    def this(name: String, gid: String, sid: Int) = 
+      this(name, gid)
+      _sid = sid
+
+  val st1 = Student("Rob", "123")
+  val st2 = Student("Rob", "123", LocalDate.now)
+  val st3 = Student("Rob", "123", 456)
+
+
+  // Objects (classes with only a single instance)
+  object StringUtils:
+    def truncate(s: String, length: Int): String = s.take(length)
+    def containsWhitespace(s: String): Boolean = s.matches(".*\\s.*")
+    def isNullOrEmpty(s: String): Boolean = s == null || s.trim.isEmpty
+
+  println(StringUtils.truncate("Bobby Dobolini", 5))
+
+  import StringUtils.*
+  println(isNullOrEmpty("JC Jenkins"))
+
+  //import StingUtils.{truncate, containsWhitespace} // import just some
+
+
+  object MathConstants:
+    val PI = 3.14159
+    val E = 2.71828
+
+  println(MathConstants.PI)
+
+
+  // Companion Objects (obj w same name as class and defined in same file)
+  case class Circle(r: Double):
+    def area: Double = Circle.calculateArea(r)
+
+  object Circle:
+    private def calculateArea(r: Double): Double = Pi * pow(r, 2.0)
+
+  val c1 = Circle(5.0)
+  println(c1.area)
+
+
+  // Other uses
